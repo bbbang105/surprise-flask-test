75c39dfd9292612cac36d426add6333f80b1595b
bbbang105 <2018111366@dgu.ac.kr>
2024-03-26 16:19:32 +0900

    feat : DataFrame 형태로 반환하도록 변경

--- association_calculator.py
+++ association_calculator.py
@@ -1,6 +1,10 @@
+import pandas as pd
+
 num_tags = 12  # 전체 태그 수
 
 def calculate_association(userInfos, exhibitionInfos):
+    association_list = []
+
     if userInfos is not None and exhibitionInfos is not None:
         for userInfo in userInfos:
             user_id = userInfo['userId']
@@ -11,6 +15,12 @@ def calculate_association(userInfos, exhibitionInfos):
                 
                 # 선택된 태그의 가중치 계산
                 weight = sum(1 for tag in user_tags if tag in exhibition_tags) / num_tags
-                print(f"User ID: {user_id}, Exhibition ID: {exhibition_id}, Weight: {weight:.3f}")
+
+                # 결과를 리스트에 저장
+                association_list.append({'userId': user_id, 'exhibitionId': exhibition_id, 'weight': weight})
+
     else:
         print("Unable to calculate association between users and exhibitions.") 
+
+    # DataFrame으로 변환하여 반환
+    return pd.DataFrame(association_list)
